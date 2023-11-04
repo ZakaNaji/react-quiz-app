a849bf6435246d8375acaffcd4921d8314cc3e5b
znaji <znaji@sqli.com>
2023-11-04 13:27:23 +0100

    quiz app static skeleton

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import MainC from "./MainC";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <MainC>
+        <p>1/15</p>
+        <p>Questions</p>
+      </MainC>
+    </div>
+  );
+}
+
+export default App;
